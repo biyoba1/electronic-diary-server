4b38b566d583868f872c4cc15cbe7a031902b62d
valery <157849728+biyoba1@users.noreply.github.com>
2024-02-07 23:43:25 +0300

    Add files via upload
    second commit

---
+++ studentBook/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"gilab.com/pragmacreviews/golang-gin-poc/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
+	routes.SubjectsRoutes(router)
+	routes.UsersRoutes(router)
+
+	router.Run(":7080")
+
+}
